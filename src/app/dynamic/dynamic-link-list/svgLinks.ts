38701685a250a847d505073ae6f404a0d191c86e
Lyapunov Mihail <lyapunovmihail@gmail.com>
2019-09-10 17:27:03 +0400

    RED3-176 правки

--- src/app/dynamic/dynamic-link-list/svgLinks.ts
+++ src/app/dynamic/dynamic-link-list/svgLinks.ts
@@ -14,20 +14,6 @@ export const SVGLINKS = [
         }
     },
     {
-        path: 'M64,0 L64,32 L32,32 L32,48 L0,48 L0,16 L48,16 L48,0 L64,0 Z',
-        transform: 'translate(196.000000, 136.000000)',
-        name: {
-            value: 'Школа №666',
-            transform: 'translate(182.500000, 139.000000) rotate(-90.000000) translate(-182.500000, -139.000000)',
-            x: '138',
-            y: '142'
-        },
-        ready: {
-            value: '0',
-            transform: 'translate(153.500000, 161.500000) rotate(-90.000000) translate(-153.500000, -161.500000) translate(132.500000, 141.500000)'
-        }
-    },
-    {
         path: 'M15,160.687274 L70,160.687274 L70,175.687274 L9.09494702e-13,175.687274 L9.09494702e-13,160.687274 L9.09494702e-13,125.687274 L15,125.687274 L15,160.687274 Z',
         transform: 'translate(0, 45)',
         name: {
@@ -73,19 +59,14 @@ export const SVGLINKS = [
         path: 'M0,0 L16,0 L16,68 L0,68 L0,0 Z',
         transform: 'translate(244.000000, 60.000000)',
         dasharray: '2',
-        stroke: '#000105',
-        opacity: '0.4',
-        fill: '#FFF',
         name: {
             value: '4',
-            opacity: '0.4',
             transform: 'translate(232.000000, 94.500000) rotate(-90.000000) translate(-232.000000, -94.500000)',
             x: '227.5',
             y: '97.5'
         },
         ready: {
             value: '0',
-            opacity: '0',
             transform: 'translate(251.500000, 34.500000) rotate(-90.000000) translate(-251.500000, -34.500000) translate(230.500000, 14.500000)'
         }
     },
@@ -93,19 +74,14 @@ export const SVGLINKS = [
         path: 'M26,0 L48,0 L48,16 L26,16 L26,37 L16,37 L16,67 L0,67 L0,23 L8,23 L8,0 L26,0 Z',
         transform: 'translate(272.000000, 154.000000)',
         dasharray: '2',
-        stroke: '#000105',
-        opacity: '0.4',
-        fill: '#FFF',
         name: {
             value: '5',
-            opacity: '0.4',
             transform: 'translate(278.000000, 231.000000) rotate(-90.000000) translate(-278.000000, -231.000000) ',
             x: '274',
             y: '234'
         },
         ready: {
             value: '0',
-            opacity: '0',
             transform: 'translate(283.500000, 262.500000) rotate(-90.000000) translate(-283.500000, -262.500000) translate(262.500000, 242.500000)'
         }
     },
@@ -113,19 +89,14 @@ export const SVGLINKS = [
         path: 'M0,67 L0,51 L16,51 L16,0 L48,0 L48,16 L32,16 L32,67 L16,67 L0,67 Z',
         transform: 'translate(320.000000, 154.000000)',
         dasharray: '2',
-        stroke: '#000105',
-        opacity: '0.4',
-        fill: '#FFF',
         name: {
             value: '6',
-            opacity: '0.4',
             transform: 'translate(334.000000, 231.500000) rotate(-90.000000) translate(-334.000000, -231.500000) ',
             x: '329.5',
             y: '234.5'
         },
         ready: {
             value: '0',
-            opacity: '0',
             transform: 'translate(335.500000, 262.500000) rotate(-90.000000) translate(-335.500000, -262.500000) translate(314.500000, 242.500000)'
         }
     },
@@ -133,19 +104,14 @@ export const SVGLINKS = [
         path: 'M0,67 L0,51 L16,51 L16,0 L48,0 L48,16 L32,16 L32,67 L16,67 L0,67 Z',
         transform: 'translate(368.000000, 154.000000)',
         dasharray: '2',
-        stroke: '#000105',
-        opacity: '0.4',
-        fill: '#FFF',
         name: {
             value: '7',
-            opacity: '0.4',
             transform: 'translate(382.000000, 232.000000) rotate(-90.000000) translate(-382.000000, -232.000000) ',
             x: '378',
             y: '235'
         },
         ready: {
             value: '0',
-            opacity: '0',
             transform: 'translate(387.500000, 262.500000) rotate(-90.000000) translate(-387.500000, -262.500000) translate(366.500000, 242.500000)'
         }
     },
@@ -153,19 +119,14 @@ export const SVGLINKS = [
         path: 'M0,0 L32,0 L32,16 L0,16 L0,0 Z',
         transform: 'translate(432.000000, 154.000000)',
         dasharray: '2',
-        stroke: '#000105',
-        opacity: '0.4',
-        fill: '#FFF',
         name: {
             value: '8',
-            opacity: '0.4',
             transform: 'translate(448.000000, 180.500000) rotate(-90.000000) translate(-448.000000, -180.500000) ',
             x: '443.5',
             y: '183.5'
         },
         ready: {
             value: '0',
-            opacity: '0',
             transform: 'translate(448.500000, 129.500000) rotate(-90.000000) translate(-448.500000, -129.500000) translate(427.500000, 109.500000)'
         }
     },
@@ -187,39 +148,43 @@ export const SVGLINKS = [
         path: 'M0,0 L112,0 L112,16 L0,16 L0,0 Z',
         transform: 'translate(296.000000, 112.000000)',
         dasharray: '2',
-        stroke: '#000105',
-        opacity: '0.4',
-        fill: '#FFF',
         name: {
             value: '10',
-            opacity: '0.4',
             transform: 'translate(352.000000, 98.000000) rotate(-90.000000) translate(-352.000000, -98.000000) ',
             x: '344',
             y: '101'
         },
         ready: {
             value: '0',
-            opacity: '0',
             transform: 'translate(352.500000, 65.500000) rotate(-90.000000) translate(-352.500000, -65.500000) translate(331.500000, 45.500000)'
+        }
+    },
+    {
+        path: 'M64,0 L64,32 L32,32 L32,48 L0,48 L0,16 L48,16 L48,0 L64,0 Z',
+        transform: 'translate(196.000000, 136.000000)',
+        name: {
+            value: 'Школа №666',
+            transform: 'translate(182.500000, 139.000000) rotate(-90.000000) translate(-182.500000, -139.000000)',
+            x: '138',
+            y: '142'
+        },
+        ready: {
+            value: '0',
+            transform: 'translate(153.500000, 161.500000) rotate(-90.000000) translate(-153.500000, -161.500000) translate(132.500000, 141.500000)'
         }
     },
     {
         path: 'M0,32 L0,16 L24,16 L24,0 L40,0 L64,0 L64,16 L40,16 L40,32 L24,32 L0,32 Z',
         transform: 'translate(458.000000, 64.000000) rotate(28.000000) translate(-458.000000, -64.000000) translate(426.000000, 48.000000)',
         dasharray: '2',
-        stroke: '#000105',
-        opacity: '0.4',
-        fill: '#FFF',
         name: {
             value: 'ДОУ №667',
-            opacity: '0.4',
             transform: 'translate(505.500000, 55.000000) rotate(-90.000000) translate(-505.500000, -55.000000) ',
             x: '469',
             y: '58'
         },
         ready: {
             value: '0',
-            opacity: '0',
             transform: 'translate(460.500000, 21.500000) rotate(-90.000000) translate(-460.500000, -21.500000) translate(439.500000, 1.500000)'
         }
     }
