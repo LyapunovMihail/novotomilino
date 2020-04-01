8cf130e530f0e52e8083f974e72c214afc31bce7
Ivan Fish <fridey0@gmail.com>
2020-04-01 13:41:57 +0400

    доб стр карантина для десктопа

---
+++ src/app/quarantine-info/quarantine-info-call/quarantine-info-call.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-quarantine-info-call',
+    templateUrl: 'quarantine-info-call.component.html',
+    styleUrls: ['./quarantine-info-call.component.scss']
+})
+
+export class QuarantineInfoCallComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
---
+++ src/app/quarantine-info/quarantine-info-online/quarantine-info-online.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-quarantine-info-online',
+    templateUrl: 'quarantine-info-online.component.html',
+    styleUrls: ['./quarantine-info-online.component.scss']
+})
+
+export class QuarantineInfoOnlineComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
