87956075556cb00a85ef9c14f1d77f1e89e06533
Ivan Fish <fridey0@gmail.com>
2020-05-12 10:37:35 +0400

    поправил появление popup'a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ export const ROOT_SELECTOR = 'app-root';
 
           <app-footer></app-footer>
 
-          <app-3red-popup></app-3red-popup>
+          <app-3red-popup *ngIf="showPopup"></app-3red-popup>
 
       </section>
 
@@ -36,6 +36,7 @@ export class AppComponent implements OnInit {
 
     public previousUrl: string;
     public currentUrl: string;
+    public showPopup;
 
     constructor(
         public appState: AppState,
@@ -45,6 +46,10 @@ export class AppComponent implements OnInit {
 
     public ngOnInit() {
         console.log('Initial App State', this.appState.state);
+
+        if (!localStorage.popup) {
+            localStorage.setItem('popup', 'true');
+        }
 
         // Подписываемся на событие смены маршрута роутера чтобы скроллить вверх страницы при смене маршрута
         this.router.events.subscribe((event) => {
@@ -59,7 +64,16 @@ export class AppComponent implements OnInit {
             window.scrollTo(0, 0);
         });
 
+        this.showOnePopup();
         // Загружаем акции для дальнейшего вычисления скидки по квартирам
         this.flatsDiscountService.getShares();
+    }
+
+    public showOnePopup() {
+
+        if (localStorage.popup === 'true') {
+            localStorage.popup = 'false';
+            this.showPopup = true;
+        }
     }
 }
