dbe29f7be5b4b113c71037cd8b4183d4736d6451
Lyapunov Mihail <lyapunovmihail@gmail.com>
2020-09-11 16:09:30 +0400

    Убрал автообновление из выгрузки

--- serv-files/serv-modules/utilits/db-cron-update.utils.ts
+++ serv-files/serv-modules/utilits/db-cron-update.utils.ts
@@ -24,10 +24,10 @@ export class DbCronUpdate {
 
     public start() {
         this.requestBase();
-        const task = new CronJob('0 13,19 * * *', () => {
-            this.requestBase();
-        }, false);
-        task.start();
+        // const task = new CronJob('0 13,19 * * *', () => {
+        //     this.requestBase();
+        // }, false);
+        // task.start();
     }
 
     public async requestBase() {
